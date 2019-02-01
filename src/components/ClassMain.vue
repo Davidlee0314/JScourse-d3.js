2cceda5f4c7ab4b89683c67ecc1bb096f3a52433
davidlee <davidlee860314@gmail.com>
2019-02-01 14:39:51 +0800

    first commit

---
+++ src/components/ClassMain.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="classMain">
+        <b-navbar toggleable="md" fixed="top" type="dark" variant="dark">
+            <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+            <b-collapse is-nav id="nav_collapse">
+                <b-navbar-nav>
+                    <b-nav-item>
+                        <router-link to="/Section2" tag="li">Section 2</router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/Section3" tag="li">Section 3</router-link>
+                    </b-nav-item>
+                    <b-nav-item>
+                        <router-link to="/Section4" tag="li">Section 4</router-link>
+                    </b-nav-item>
+                </b-navbar-nav>
+            </b-collapse>
+        </b-navbar>
+        <router-view></router-view>
+    </div>
+</template>
---
+++ src/components/Section2.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="section2">
+        <h3>Section 2</h3>
+    </div>
+</template>
